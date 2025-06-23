9a6893b2e1e86d190a72c40313c2237ead69f66a
hungnguyenuet <hungnguyenuet@gmail.com>
2025-06-23 21:49:35 +0700

    Add GitHub OAuth2 support with user registration and authentication handling

--- auth-service/src/main/java/vti/dtn/auth_service/oauth2/common/OAuth2Constant.java
+++ auth-service/src/main/java/vti/dtn/auth_service/oauth2/common/OAuth2Constant.java
@@ -4,4 +4,6 @@ public class OAuth2Constant {
     public static final String GOOGLE = "google";
     public static final String FACEBOOK = "facebook";
     public static final String GITHUB = "github";
+
+    public static final String ROLE_USER = "ROLE_USER";
 }
